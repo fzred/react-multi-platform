7190e88f2db2b3d991119417fe87624686de66f7
fzred <1179750402@qq.com>
2016-09-25 23:40:01 +0800

    flag

--- app/common/Home/components/Type1.js
+++ app/common/Home/components/Type1.js
@@ -1,37 +1,10 @@
-import React, { Component } from 'react'
+import React, { PropTypes } from 'react'
 import {
   StyleSheet,
-  Navigator,
-  Text,
-  View,
-  TouchableOpacity,
-  ScrollView,
   Image,
   Dimensions,
-  PixelRatio,
 } from 'react-native'
 import Swiper from 'react-native-swiper'
-
-export default class extends Component {
-  render() {
-    return (
-      <Swiper style={styles.wrapper}
-        showsButtons={false}
-        autoplay
-        height={imgHeight}
-      >
-        {
-          this.props.item.list.map((item, i) => {
-            return (<Image source={{ uri: item.acturl }}
-              style={styles.img}
-              key={i}
-            />)
-          })
-        }
-      </Swiper>
-    )
-  }
-}
 
 const width = Dimensions.get('window').width
 const imgHeight = width * 0.64
@@ -45,3 +18,30 @@ const styles = StyleSheet.create({
     height: imgHeight,
   },
 })
+
+function Type1({ item }) {
+  return (
+    <Swiper
+      style={styles.wrapper}
+      showsButtons={false}
+      autoplay
+      height={imgHeight}
+    >
+      {
+        item.list.map((val, i) => (
+          <Image
+            source={{ uri: val.acturl }}
+            style={styles.img}
+            key={i}
+          />)
+        )
+      }
+    </Swiper>
+  )
+}
+
+Type1.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default Type1
