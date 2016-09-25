3daad30aa0bf0ef6367b83c3d1fa83452694b2a1
fzred <1179750402@qq.com>
2016-09-26 00:07:35 +0800

    flag

--- app/common/Home/Home.js
+++ app/common/Home/Home.js
@@ -13,6 +13,28 @@ import Type2 from './components/Type2'
 import Type10 from './components/Type10'
 import Type14 from './components/Type14'
 
+const styles = StyleSheet.create({
+  wrap: {
+    backgroundColor: styleColor.C10,
+  },
+})
+
+
+function renderMudule(item, i) {
+  switch (item.mtype) {
+    case 1:
+      return <Type1 key={i} item={item} />
+    case 2:
+      return <Type2 key={i} item={item} />
+    case 10:
+      return <Type10 key={i} item={item} />
+    case 14:
+      return <Type14 key={i} item={item} />
+    default:
+      return null
+  }
+}
+
 export default class extends Component {
   constructor() {
     super()
@@ -26,25 +48,10 @@ export default class extends Component {
     const data = await fetch('http://m.allpyra.com/api/main/queryMain.jsp?op=1&pageNo=1&pageSize=50&ver=1&actid=1504081630530004')
       .then(req => req.json())
     this.setState({
-      muduleList: data.obj.list
+      muduleList: data.obj.list,
     })
-    console.log(data)
   }
 
-  renderMudule(item, i) {
-    switch (item.mtype) {
-      case 1:
-        return <Type1 key={i} item={item}/>
-      case 2:
-        return <Type2 key={i} item={item}/>
-      case 10:
-        return <Type10 key={i} item={item}/>
-      case 14:
-        return <Type14 key={i} item={item}/>
-      default:
-        return null
-    }
-  }
 
   render() {
     if (!this.state.muduleList) {
@@ -57,14 +64,9 @@ export default class extends Component {
     return (
       <ScrollView style={styles.wrap}>
         <View>
-          {this.state.muduleList.map(this.renderMudule.bind(this))}
+          {this.state.muduleList.map(renderMudule)}
         </View>
       </ScrollView>
     )
   }
 }
-const styles = StyleSheet.create({
-  wrap: {
-    backgroundColor: styleColor.C10,
-  }
-})
