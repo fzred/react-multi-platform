ebff0accf6fddc145e53bad13927022c2291d6ba
fzred <1179750402@qq.com>
2016-09-27 14:14:32 +0800

    update

--- app/common/Main.js
+++ app/common/Main.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Navigator,
-} from 'react-native'
 import Home from './Home/Home'
 import Category from './Category'
 import NavTabBar from './NavTabBar/'
 
-export default class extends Component {
-  constructor() {
-    super()
-  }
-
-  render() {
-    return (
-      <ScrollableTabView renderTabBar={() => <NavTabBar/>} tabBarPosition="bottom">
-        <Home tabLabel="Home"></Home>
-        <Category tabLabel="Category"></Category>
-      </ScrollableTabView>
-    )
+export default function Main() {
+  return (
+    <ScrollableTabView renderTabBar={() => <NavTabBar />} tabBarPosition="bottom">
+      <Home tabLabel="Home" />
+      <Category tabLabel="Category" />
+    </ScrollableTabView>
+  )
 //    return (
 //      <Navigator
 //        initialRoute={{ name: 'Home', component: Home }}
@@ -34,24 +22,4 @@ export default class extends Component {
 //          return <Component {...route.params} navigator={navigator}/>
 //        }}/>
 //    )
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-})
