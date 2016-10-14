c068216324569c18248bae81224b21bf2fd77c24
fzred <1179750402@qq.com>
2016-10-14 20:33:17 +0800

    px2rem

--- web/common/px2rem.js
+++ web/common/px2rem.js
@@ -1,7 +1,7 @@
 let px2rem = function (px) { // eslint-disable-line
   return px / 75
 }
-if (typeof window !== 'undefined') {
+if (process.env.BROWSER) {
   px2rem = (px) => window.lib.flexible.px2rem(px)
 }
 export default px2rem
