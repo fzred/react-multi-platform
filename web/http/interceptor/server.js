ee131fe0fd80fff2da2149cfab7d2594badd2f72
fzred <1179750402@qq.com>
2016-10-18 11:43:33 +0800

    update

--- web/http/interceptor/server.js
+++ web/http/interceptor/server.js
@@ -1,9 +1,12 @@
+/*
+注册服务端 rend 拦截器
+ */
 export default function (rend, { req }) {
-  if (req) {
-    rend.interceptors.request.push(requestConfig => {
-      const request = Object.assign({}, requestConfig)
+  rend.interceptors.request.push(requestConfig => {
+    const request = Object.assign({}, requestConfig)
+    if (req && req.headers) {
       request.headers.set('cookie', req.headers.cookie)
-      return request
-    })
-  }
+    }
+    return request
+  })
 }
