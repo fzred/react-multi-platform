90327135c04a912a0627f1aa95b5d617c895590f
fzred <1179750402@qq.com>
2016-10-19 08:19:43 +0800

    update

--- web/store/logger/logger.server.js
+++ web/store/logger/logger.server.js
@@ -5,9 +5,10 @@ export default function createLogger() {
   return store => next => action => { // eslint-disable-line no-unused-vars
     const formattedPayload = inspect(action.data, { // eslint-disable-line no-unused-vars
       colors: true,
+      showHidden: true,
     })
-    // console.log(` * ${action.type}: ${formattedPayload}`) // eslint-disable-line no-console
-    console.log(` * ${action.type}`) // eslint-disable-line no-console
+    console.log(` * ${action.type}: ${formattedPayload}`) // eslint-disable-line no-console
+    // console.log(` * ${action.type}`) // eslint-disable-line no-console
     return next(action)
   }
 }
