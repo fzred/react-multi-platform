72807e669db23aacae5934c6993292e59c63e771
fzred <1179750402@qq.com>
2016-10-30 17:38:31 +0800

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,12 @@ import { getUsersInfo } from '../../common/actions/user'
 
 /* eslint-disable global-require */
 
-// The top-level (parent) route
 export default {
 
   path: '/',
 
-  // Keep in mind, routes are evaluated in order
   children: [
     require('./home').default,
-    // require('./user').default,
-    // // require('./contact').default,
     require('./login').default,
   ],
 
@@ -36,14 +32,9 @@ export default {
   async action({ next }) {
     let route
 
-    // Execute each child route until one of them return the result
-    // TODO: move this logic to the `next` function
     do {
       route = await next()
     } while (!route)
-    // Provide default values for title, description etc.
-    route.title = `${route.title || 'Untitled Page'} - 金字塔`
-    route.description = route.description || ''
 
     return route
   },
