8cae58c0c35d6326f96b7836ae3934c95de4af2c
fzred <1179750402@qq.com>
2016-10-31 22:57:52 +0800

    loading 组件

---
+++ web/components/Loading/index.js
@@ -0,0 +1,41 @@
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import withStyles from 'isomorphic-style-loader/lib/withStyles'
+import cns from 'classnames'
+import s from './index.css'
+
+function Loading({ loadingCount }) {
+  return (
+    <div className={cns(s.root, loadingCount < 1 ? 'hide' : '')}>
+      <div className={s.spinner}>
+        <div className={cns(s.spinnerContainer, s.container1)}>
+          <div className={cns(s.circle1)} />
+          <div className={cns(s.circle2)} />
+          <div className={cns(s.circle3)} />
+          <div className={cns(s.circle4)} />
+        </div>
+        <div className={cns(s.spinnerContainer, s.container2)}>
+          <div className={cns(s.circle1)} />
+          <div className={cns(s.circle2)} />
+          <div className={cns(s.circle3)} />
+          <div className={cns(s.circle4)} />
+        </div>
+        <div className={cns(s.spinnerContainer, s.container3)}>
+          <div className={cns(s.circle1)} />
+          <div className={cns(s.circle2)} />
+          <div className={cns(s.circle3)} />
+          <div className={cns(s.circle4)} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Loading.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  loadingCount: PropTypes.number.isRequired,
+}
+
+export default connect(state => ({
+  loadingCount: state.loadingCount,
+}))(withStyles(s)(Loading))
